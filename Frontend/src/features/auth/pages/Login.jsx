62f59f4431e5ec1c56893862ce57cd684c6cf5b7
Aman Tomar <amantoma700@gmail.com>
2026-06-20 23:17:20 +0530

    added authentication page styling

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,13 +1,18 @@
-
+import "../../auth/auth.form.scss"
 
 const Login = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
   return (
     <main>
       <div className="form-container">
         <h1>Login</h1>
 
-        <form>
-          <div className="input-group">
+        <form onSubmit={handleSubmit}>
+          <div className=" input-group">
             <label htmlFor="email">Email</label>
             <input type="email" id="email" name="email" placeholder="Enter your email" required />
           </div>
@@ -15,8 +20,10 @@ const Login = () => {
             <label htmlFor="password">Password</label>
             <input type="password" id="password" name="password" placeholder="Enter your password" required />
           </div>
-          <button type="submit">Login</button>
+          <button className="button primary-button" type="submit" >Login</button>
         </form>
+
+        <p>Don't have an account? <a href="/register">Register</a> </p>
       </div>
     </main>
   )
